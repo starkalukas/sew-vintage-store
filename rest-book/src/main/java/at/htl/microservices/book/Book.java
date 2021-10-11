4932068627a544a8b3ac0162b45bf0cc0c06ad18
StarkaLukas <43465679+StarkaLukas@users.noreply.github.com>
2021-10-11 14:29:36 +0200

    Update Book.java

--- rest-book/src/main/java/at/htl/microservices/book/Book.java
+++ rest-book/src/main/java/at/htl/microservices/book/Book.java
@@ -1,13 +1,20 @@
 package at.htl.microservices.book;
 
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.json.bind.annotation.JsonbProperty;
 import java.time.Instant;
 
 public class Book {
+
+    @JsonbProperty("isbn_13")
     public String isbn13;
     public String title;
     public String author;
+    @JsonbProperty("year_of_publication")
     public int yearOfPublication;
     public String genre;
+    @JsonbProperty("creation_time")
+    @JsonbDateFormat("yyyy-MM-dd")
     public Instant creationTime;
 
     @Override
